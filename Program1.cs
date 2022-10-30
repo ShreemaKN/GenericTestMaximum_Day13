642b80289109bb7a9a070a4f39e0eabf278405cc
Shreema KN <shreemakn1013@gmail.com>
2022-10-31 04:29:11 +0530

    added items

---
+++ Program1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericFindMax
+{
+    public class Program1
+    {
+        static void Main(string[] args)
+        {
+            FindMaxFloat findMaxfloat = new FindMaxFloat();
+            findMaxfloat.MaximumFloat(21.3, 4.5, 6.7);
+        }
+    }
+}
